bee21174fe0a2ab33af0b148dbbdbd1858897a6d
Ramprit Sahani <21260321+Ramprits@users.noreply.github.com>
2021-02-03 08:28:35 +0530

    Adding redux provider

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
 import theme from "./theme/theme";
-import { Store, persistor } from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 const rootElement = document.querySelector("#root");
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
-
-    <Provider store={Store}>
+    <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
+    ;
   </ThemeProvider>,
   rootElement
 );
